539c86cf4c4ffbc2c7f893147d6b45371c2ebc71
Rita Sitnova <marg.sitnova@gmail.com>
2022-06-05 22:06:49 +0300

    added AnimalTest class

---
+++ src/test/java/com/example/AnimalTest.java
@@ -0,0 +1,49 @@
+package com.example;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
+public class AnimalTest { // создали тестовый класс
+
+        private final String kindOfAnimal;
+        private final List<String> expected; // создали поля тестового класса
+
+        public AnimalTest(String kindOfAnimal, List<String> expected) {
+            this.kindOfAnimal = kindOfAnimal;
+            this.expected = expected; // создали конструктор тестового класса
+        }
+
+        @Parameterized.Parameters
+        public static Object[] getListOfMealDataForHerbivoreAndPredator() {
+            return new Object[][] {
+                    {"Травоядное", List.of("Трава", "Различные растения")},
+                    {"Хищник", List.of("Животные", "Птицы", "Рыба")},
+                    {"Рептилия", List.of("1", "2", "3")} // передали тестовые данные
+            };
+        }
+
+        @Test
+        public void testFoodForHerbivoreAndPredator() throws Exception {
+            Animal animal = new Animal(); // создали экземпляр класса
+            try {
+                List<String> actual = animal.getFood(kindOfAnimal); // обратились к полям тестового класса
+                assertEquals(expected, actual); // сравнили ожидаемый и фактический результат
+            }
+            catch (Exception exception) {
+                System.out.println("Произошла ошибка!");
+            }
+        }
+
+        @Test
+        public void shouldReturnListOfFamily() {
+            Animal animal = new Animal(); // создали экземпляр класса
+            String actual = animal.getFamily(); // вызвали проверяемый метод
+            String expected = "Существует несколько семейств: заячьи, беличьи, мышиные, кошачьи, псовые, медвежьи, куньи";
+            assertEquals(expected, actual); // сравнили ожидаемый результат с фактическим
+    }
+}
